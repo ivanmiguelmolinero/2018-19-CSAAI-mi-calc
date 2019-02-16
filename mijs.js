d3fb1f9dd66b9ab0b59dbbaf8975cca2849f00aa
ivanmiguelmolinero <i.miguel.2016@alumnos.urjc.es>
2019-02-16 13:03:10 +0100

    Ya escribe todos los números sin problema

--- mijs.js
+++ mijs.js
@@ -1,20 +1,143 @@
 function main() {
   console.log("Que comiencen los juegos de JavaScript");
 
-  var boton = document.getElementById('boton')
+  var boton = document.getElementById('boton');
 
-  boton.onclick = () => {
-    console.log("Click");
+  boton7.onclick = () => {
 
     var display = document.getElementById('display');
-    var displaybien = display.innerHTML.replace("0","")
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","7");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton8.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","8");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton9.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","9");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton4.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","4");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton5.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","5");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton6.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","6");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton1.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
     var displayseguido = displaybien.concat("","1");
     display.innerHTML = displayseguido;
+
   }
+
+  boton3.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","3");
+    display.innerHTML = displayseguido;
+
+  }
+
+  boton0.onclick = () => {
+
+    var display = document.getElementById('display');
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
+    var displayseguido = displaybien.concat("","0");
+    display.innerHTML = displayseguido;
+
+  }
+
   boton2.onclick = () => {
 
     var display = document.getElementById('display');
-    var displaybien = display.innerHTML.replace("0","")
+    var pos = display.innerHTML.indexOf("0");
+    if (pos == 0) {
+      var displaybien = display.innerHTML.replace("0","");
+    } else {
+      var displaybien = display.innerHTML;
+    }
     var displayseguido = displaybien.concat("","2");
     display.innerHTML = displayseguido;
 
